ad5cfe5bb3cd193ab73a9228b1a54d7153dccf0e
w-hosinski <woj.hosinski@gmail.com>
2021-10-06 18:28:19 +0200

    semikolons entfernen
    Semikolons entfernen da man mit typescript diese nicht mehr braucht

--- index.js
+++ index.js
@@ -1,229 +1,229 @@
-var input = document.getElementById("input");
-var result = document.getElementById("result");
-var inputType = document.getElementById("inputType");
-var resultType = document.getElementById("resultType");
-var button = document.getElementById("swap");
-var convSelect = document.getElementById("convSelect");
-var inputTypeValue = inputType.value;
-var resultTypeValue = resultType.value;
-var inputMult;
-var resultMult;
+var input = document.getElementById("input")
+var result = document.getElementById("result")
+var inputType = document.getElementById("inputType")
+var resultType = document.getElementById("resultType")
+var button = document.getElementById("swap")
+var convSelect = document.getElementById("convSelect")
+var inputTypeValue = inputType.value
+var resultTypeValue = resultType.value
+var inputMult
+var resultMult
 
-input.addEventListener("keyup",converter);
-inputType.addEventListener("change",converter);
-resultType.addEventListener("change",converter);
-button.addEventListener("click",swaping);
-button.addEventListener("click",converter);
-convSelect.addEventListener("change",selector);
-convSelect.addEventListener("change",converter);
+input.addEventListener("keyup",converter)
+inputType.addEventListener("change",converter)
+resultType.addEventListener("change",converter)
+button.addEventListener("click",swaping)
+button.addEventListener("click",converter)
+convSelect.addEventListener("change",selector)
+convSelect.addEventListener("change",converter)
 
 function selector(){
     if(convSelect.value == "energy"){
-        document.getElementById("option1_1").innerHTML = "Joule (J)";
-        document.getElementById("option1_2").innerHTML = "Kilojoule (kJ)";
-        document.getElementById("option1_3").innerHTML = "Kilowatt-Hour (kW*h)";
-        document.getElementById("option1_4").innerHTML = "Megawatt-Hour (MW*h)";
-        document.getElementById("option1_5").innerHTML = "British thermal unit (Btu)";
-        document.getElementById("option1_6").innerHTML = "Million Btu (MMBtu)";
-        document.getElementById("option1_7").innerHTML = "Barrel of Oil Equiv (BOE)";
-        document.getElementById("option1_8").innerHTML = "Kilocalorie (kcal)";
-        document.getElementById("option1_9").innerHTML = "Tonne Of Coal Equiv (TCE)";
-        document.getElementById("option2_1").innerHTML = "Joule (J)";
-        document.getElementById("option2_2").innerHTML = "Kilojoule (kJ)";
-        document.getElementById("option2_3").innerHTML = "Kilowatt-Hour (kW*h)";
-        document.getElementById("option2_4").innerHTML = "Megawatt-Hour (MW*h)";
-        document.getElementById("option2_5").innerHTML = "British thermal unit (Btu)";
-        document.getElementById("option2_6").innerHTML = "Million Btu (MMBtu)";
-        document.getElementById("option2_7").innerHTML = "Barrel of Oil Equiv (BOE)";
-        document.getElementById("option2_8").innerHTML = "Kilocalorie (kcal)";
-        document.getElementById("option2_9").innerHTML = "Tonne Of Coal Equiv (TCE)";
-        document.getElementById("option1_1").value = "joule";
-        document.getElementById("option1_2").value = "kilojoule";
-        document.getElementById("option1_3").value = "kwh";
-        document.getElementById("option1_4").value = "mwh";
-        document.getElementById("option1_5").value = "btu";
-        document.getElementById("option1_6").value = "mmbtu";
-        document.getElementById("option1_7").value = "boe";
-        document.getElementById("option1_8").value = "kcal";
-        document.getElementById("option1_9").value = "tce";
-        document.getElementById("option1_9").style = "display:initial";
-        document.getElementById("option2_1").value = "joule";
-        document.getElementById("option2_2").value = "kilojoule";
-        document.getElementById("option2_3").value = "kwh";
-        document.getElementById("option2_4").value = "mwh";
-        document.getElementById("option2_5").value = "btu";
-        document.getElementById("option2_6").value = "mmbtu";
-        document.getElementById("option2_7").value = "boe";
-        document.getElementById("option2_8").value = "kcal";
-        document.getElementById("option2_9").value = "tce";
-        document.getElementById("option2_9").style = "display:initial";
+        document.getElementById("option1_1").innerHTML = "Joule (J)"
+        document.getElementById("option1_2").innerHTML = "Kilojoule (kJ)"
+        document.getElementById("option1_3").innerHTML = "Kilowatt-Hour (kW*h)"
+        document.getElementById("option1_4").innerHTML = "Megawatt-Hour (MW*h)"
+        document.getElementById("option1_5").innerHTML = "British thermal unit (Btu)"
+        document.getElementById("option1_6").innerHTML = "Million Btu (MMBtu)"
+        document.getElementById("option1_7").innerHTML = "Barrel of Oil Equiv (BOE)"
+        document.getElementById("option1_8").innerHTML = "Kilocalorie (kcal)"
+        document.getElementById("option1_9").innerHTML = "Tonne Of Coal Equiv (TCE)"
+        document.getElementById("option2_1").innerHTML = "Joule (J)"
+        document.getElementById("option2_2").innerHTML = "Kilojoule (kJ)"
+        document.getElementById("option2_3").innerHTML = "Kilowatt-Hour (kW*h)"
+        document.getElementById("option2_4").innerHTML = "Megawatt-Hour (MW*h)"
+        document.getElementById("option2_5").innerHTML = "British thermal unit (Btu)"
+        document.getElementById("option2_6").innerHTML = "Million Btu (MMBtu)"
+        document.getElementById("option2_7").innerHTML = "Barrel of Oil Equiv (BOE)"
+        document.getElementById("option2_8").innerHTML = "Kilocalorie (kcal)"
+        document.getElementById("option2_9").innerHTML = "Tonne Of Coal Equiv (TCE)"
+        document.getElementById("option1_1").value = "joule"
+        document.getElementById("option1_2").value = "kilojoule"
+        document.getElementById("option1_3").value = "kwh"
+        document.getElementById("option1_4").value = "mwh"
+        document.getElementById("option1_5").value = "btu"
+        document.getElementById("option1_6").value = "mmbtu"
+        document.getElementById("option1_7").value = "boe"
+        document.getElementById("option1_8").value = "kcal"
+        document.getElementById("option1_9").value = "tce"
+        document.getElementById("option1_9").style = "display:initial"
+        document.getElementById("option2_1").value = "joule"
+        document.getElementById("option2_2").value = "kilojoule"
+        document.getElementById("option2_3").value = "kwh"
+        document.getElementById("option2_4").value = "mwh"
+        document.getElementById("option2_5").value = "btu"
+        document.getElementById("option2_6").value = "mmbtu"
+        document.getElementById("option2_7").value = "boe"
+        document.getElementById("option2_8").value = "kcal"
+        document.getElementById("option2_9").value = "tce"
+        document.getElementById("option2_9").style = "display:initial"
         
     }
     else if(convSelect.value == "volume") {
-        document.getElementById("option1_1").innerHTML = "Cubic Meter (m³)";
-        document.getElementById("option1_2").innerHTML = "Barrel (bbl)";
-        document.getElementById("option1_3").innerHTML = "Cubic Foot (ft³)";
-        document.getElementById("option1_4").innerHTML = "Liter (l)";
-        document.getElementById("option1_5").innerHTML = "Gallon (gal)";
-        document.getElementById("option1_6").innerHTML = "Cubic Inch (in³)";
-        document.getElementById("option1_7").innerHTML = "Cubic Centimeter (cm³)";
-        document.getElementById("option1_8").innerHTML = "Board Foot (bf)";
-        document.getElementById("option1_9").innerHTML = "";
-        document.getElementById("option2_1").innerHTML = "Cubic Meter (m³)";
-        document.getElementById("option2_2").innerHTML = "Barrel (bbl)";
-        document.getElementById("option2_3").innerHTML = "Cubic Foot (ft³)";
-        document.getElementById("option2_4").innerHTML = "Liter (l)";
-        document.getElementById("option2_5").innerHTML = "Gallon (gal)";
-        document.getElementById("option2_6").innerHTML = "Cubic Inch (in³)";
-        document.getElementById("option2_7").innerHTML = "Cubic Centimeter (cm³)";
-        document.getElementById("option2_8").innerHTML = "Board Foot (bf)";
-        document.getElementById("option2_9").innerHTML = "";
-        document.getElementById("option1_1").value = "cubicmeter";
-        document.getElementById("option1_2").value = "barrel";
-        document.getElementById("option1_3").value = "cubicfoot";
-        document.getElementById("option1_4").value = "liter";
-        document.getElementById("option1_5").value = "gallon";
-        document.getElementById("option1_6").value = "cubicinch";
-        document.getElementById("option1_7").value = "cubiccentimeter";
-        document.getElementById("option1_8").value = "boardfoot";
-        document.getElementById("option1_9").value = "gay";
-        document.getElementById("option1_9").style = "display:none";
-        document.getElementById("option2_1").value = "cubicmeter";
-        document.getElementById("option2_2").value = "barrel";
-        document.getElementById("option2_3").value = "cubicfoot";
-        document.getElementById("option2_4").value = "liter";
-        document.getElementById("option2_5").value = "gallon";
-        document.getElementById("option2_6").value = "cubicinch";
-        document.getElementById("option2_7").value = "cubiccentimeter";
-        document.getElementById("option2_8").value = "boardfoot";
-        document.getElementById("option2_9").value = "gay";
-        document.getElementById("option2_9").style = "display:none";
+        document.getElementById("option1_1").innerHTML = "Cubic Meter (m³)"
+        document.getElementById("option1_2").innerHTML = "Barrel (bbl)"
+        document.getElementById("option1_3").innerHTML = "Cubic Foot (ft³)"
+        document.getElementById("option1_4").innerHTML = "Liter (l)"
+        document.getElementById("option1_5").innerHTML = "Gallon (gal)"
+        document.getElementById("option1_6").innerHTML = "Cubic Inch (in³)"
+        document.getElementById("option1_7").innerHTML = "Cubic Centimeter (cm³)"
+        document.getElementById("option1_8").innerHTML = "Board Foot (bf)"
+        document.getElementById("option1_9").innerHTML = ""
+        document.getElementById("option2_1").innerHTML = "Cubic Meter (m³)"
+        document.getElementById("option2_2").innerHTML = "Barrel (bbl)"
+        document.getElementById("option2_3").innerHTML = "Cubic Foot (ft³)"
+        document.getElementById("option2_4").innerHTML = "Liter (l)"
+        document.getElementById("option2_5").innerHTML = "Gallon (gal)"
+        document.getElementById("option2_6").innerHTML = "Cubic Inch (in³)"
+        document.getElementById("option2_7").innerHTML = "Cubic Centimeter (cm³)"
+        document.getElementById("option2_8").innerHTML = "Board Foot (bf)"
+        document.getElementById("option2_9").innerHTML = ""
+        document.getElementById("option1_1").value = "cubicmeter"
+        document.getElementById("option1_2").value = "barrel"
+        document.getElementById("option1_3").value = "cubicfoot"
+        document.getElementById("option1_4").value = "liter"
+        document.getElementById("option1_5").value = "gallon"
+        document.getElementById("option1_6").value = "cubicinch"
+        document.getElementById("option1_7").value = "cubiccentimeter"
+        document.getElementById("option1_8").value = "boardfoot"
+        document.getElementById("option1_9").value = "gay"
+        document.getElementById("option1_9").style = "display:none"
+        document.getElementById("option2_1").value = "cubicmeter"
+        document.getElementById("option2_2").value = "barrel"
+        document.getElementById("option2_3").value = "cubicfoot"
+        document.getElementById("option2_4").value = "liter"
+        document.getElementById("option2_5").value = "gallon"
+        document.getElementById("option2_6").value = "cubicinch"
+        document.getElementById("option2_7").value = "cubiccentimeter"
+        document.getElementById("option2_8").value = "boardfoot"
+        document.getElementById("option2_9").value = "gay"
+        document.getElementById("option2_9").style = "display:none"
     }
 }
 
 function swaping(){
-    [inputType.value, resultType.value] = [resultType.value, inputType.value];
+    [inputType.value, resultType.value] = [resultType.value, inputType.value]
 }
 
 function converter(){
-    inputTypeValue = inputType.value;
-    resultTypeValue = resultType.value;
+    inputTypeValue = inputType.value
+    resultTypeValue = resultType.value
     
 switch(inputTypeValue){
     case "cubicmeter":
-        inputMult = 1000;    
-        break;
+        inputMult = 1000    
+        break
     case "barrel":
-        inputMult = 158.987294928;
-        break;
+        inputMult = 158.987294928
+        break
     case "cubicfoot":
-        inputMult = 28.316864592; 
-        break;
+        inputMult = 28.316864592 
+        break
     case "liter":
-        inputMult = 1;
-        break;
+        inputMult = 1
+        break
     case "gallon":
-        inputMult = 3.785411784;
-        break;
+        inputMult = 3.785411784
+        break
     case "cubicinch":
-        inputMult = 0.016387064;
-        break;
+        inputMult = 0.016387064
+        break
     case "cubiccentimeter":
-        inputMult = 0.001;
-        break;    
+        inputMult = 0.001
+        break    
     case "boardfoot":
-        inputMult = 2.3597372;
-        break;
+        inputMult = 2.3597372
+        break
     case "gay":
-        inputMult = 0;
-        break;
+        inputMult = 0
+        break
     case "joule":
-        inputMult = 0.001;    
-        break;
+        inputMult = 0.001    
+        break
     case "kilojoule":
-        inputMult = 1;
-        break;
+        inputMult = 1
+        break
     case "kwh":
-        inputMult = 3600; 
-        break;
+        inputMult = 3600 
+        break
     case "mwh":
-        inputMult = 3600000;
-        break;
+        inputMult = 3600000
+        break
     case "btu":
-        inputMult = 1.05506;
-        break;
+        inputMult = 1.05506
+        break
     case "mmbtu":
-        inputMult = 1055060;
-        break;
+        inputMult = 1055060
+        break
     case "boe":
-        inputMult = 6117863.2;
-        break;    
+        inputMult = 6117863.2
+        break    
     case "kcal":
-        inputMult = 4.184;
-        break;
+        inputMult = 4.184
+        break
     case "tce":
-        inputMult = 29307600;
-        break;
+        inputMult = 29307600
+        break
 }
 switch(resultTypeValue){
     case "cubicmeter":
-        resultMult = 1000;    
-        break;
+        resultMult = 1000    
+        break
     case "barrel":
-        resultMult = 158.987294928;
-        break;
+        resultMult = 158.987294928
+        break
     case "cubicfoot":
-        resultMult = 28.316864592; 
-        break;
+        resultMult = 28.316864592 
+        break
     case "liter":
-        resultMult = 1;
-        break;
+        resultMult = 1
+        break
     case "gallon":
-        resultMult = 3.785411784;
-        break;
+        resultMult = 3.785411784
+        break
     case "cubicinch":
-        resultMult = 0.016387064;
-        break;
+        resultMult = 0.016387064
+        break
     case "cubiccentimeter":
-        resultMult = 0.001;
-        break;    
+        resultMult = 0.001
+        break    
     case "boardfoot":
-        resultMult = 2.3597372;
-        break;
+        resultMult = 2.3597372
+        break
     case "gay":
-        resultMult = 0;
-        break;
+        resultMult = 0
+        break
     case "joule":
-        resultMult = 0.001;    
-        break;
+        resultMult = 0.001    
+        break
     case "kilojoule":
-        resultMult = 1;
-        break;
+        resultMult = 1
+        break
     case "kwh":
-        resultMult = 3600; 
-        break;
+        resultMult = 3600 
+        break
     case "mwh":
-        resultMult = 3600000;
-        break;
+        resultMult = 3600000
+        break
     case "btu":
-        resultMult = 1.05506;
-        break;
+        resultMult = 1.05506
+        break
     case "mmbtu":
-        resultMult = 1055060;
-        break;
+        resultMult = 1055060
+        break
     case "boe":
-        resultMult = 6117863.2;
-        break;    
+        resultMult = 6117863.2
+        break    
     case "kcal":
-        resultMult = 4.184;
-        break;
+        resultMult = 4.184
+        break
     case "tce":
-        resultMult = 29307600;
-        break;
+        resultMult = 29307600
+        break
 }
-var temp = input.value*(inputMult/resultMult);
-result.value = temp.toFixed(3);
+var temp = input.value*(inputMult/resultMult)
+result.value = temp.toFixed(3)
 }
 
-//[a, b] = [b, a];
+//[a, b] = [b, a]
 
